039428b5fa018c9683b9a8aac521dbde7c591b6e
Mathias Kristiansen <matkri3110@gmail.com>
2025-01-21 19:07:41 +0100

    Laget en maintag og en seksjon med id-er slik at vi kan skrive direkte fra javascript + laget variabler for navMenu og ressurser som linker til id-ene

---
+++ Arbeidskrav_1/script.js
@@ -0,0 +1,10 @@
+console.log(resources) // Logger ut resources.js i konsollen slik at vi kan bruke innholdet i koden.
+
+const navMenu = document.getElementById("nav-menu") // Lager en variabel for navigasjonsmenyen og linker den til id-en som er laget i HTML-filen.
+const resourcesSection = document.getElementById("resources") // Lager en variabel for ressursseksjonen og linker den til id-en som er laget i HTML-filen.
+
+
+
+
+
+    
