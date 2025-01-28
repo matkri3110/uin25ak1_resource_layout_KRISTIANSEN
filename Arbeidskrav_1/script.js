0b1a3365f345813bdf339c929df19eacdc5089cb
Mathias Kristiansen <matkri3110@gmail.com>
2025-01-28 20:49:34 +0100

    Oppdatering til kode, laget helt nytt javascript, en del ny css og oppdatert html til å passe den nye koden.

--- Arbeidskrav_1/script.js
+++ Arbeidskrav_1/script.js
@@ -1,36 +1,43 @@
 console.log(resources) // Logger ut resources.js i konsollen slik at vi kan bruke innholdet i koden.
 
-const navMenu = document.getElementById("nav-menu") // Lager en variabel for navigasjonsmenyen og linker den til id-en som er laget i HTML-filen.
-const resourcesSection = document.getElementById("resources") // Lager en variabel for ressursseksjonen og linker den til id-en som er laget i HTML-filen.
+let navbarHTML = "" // Lager en tom string som vi skal fylle med HTML for navigasjonsmenyen.
 
 /*
-    Lager en funksjon for å vise navigasjonsmenyen som er linket til de ulike kategoriene i resources. 
-
+    Lager navigasjonsknapper for hver kategori i resource-arrayet.
 */
+resources.map(resource => navbarHTML += 
+    `
+        <ul id="menu">
+            <li>
+                <button id="menu" onclick="content('${resource.category}')">${resource.category}</button>
+            </li>
+        </ul>
+    ` 
+)
 
-//navMenu inneholder en liste over kategoriene i resources.js
-
-const categories = [...new Set(resources.map(item => item.category))]
-categories.forEach(category => {
-    const menuItem = document.createElement("li")
-    const menuLink = document.createElement("a")
-    menuLink.textContent = category
-    menuLink.href = "#"
-
-    menuLink.addEventListener("click", () => {
-        showCategory(category)
-    })
-
-    menuItem.appendChild(menuLink)
-    navMenu.appendChild(menuItem)
-})
-
+document.getElementById("menu").innerHTML = navbarHTML // Oppdaterer innholdet i navbarHTML.
 
 /*
-    Neste steg vil være å lage en funksjon som viser ressursene som er linket til de ulike kategoriene.
-
-    Funksjonen skal gjøre følgende:
-    - Tømme tidligere innhold.
-    - Legge til en tittel, beskrivelse og en liste med lenker for ressursene.
+    Etter at vi har laget navigasjonsknappene skal vi lage en funksjon som viser innholdet til den tilhørende kategorien. 
+    Så først vil vi filtrere resource-arrayet for å finne valgt kategori.
+    Så skal vi oppdatere innholdet på siden basert på valgt kategori.
+    Innholdet skal bestå av en overskrift, en beskrivelse og lenker til ulike ressurser som kan trykkes på.
 */
+function content(category) {
+    const filteredResources = resources.filter (item => item.category === category) // Filtrer ut ressurser som har samme kategori som den vi trykket på.
+    const toggledResource = filteredResources [0]
+    const contentHTML = 
+    // Lager en template literal som inneholder HTML struktur og nøkler for innholdet vi vil vise.
+    `
+        <h2>${toggledResource.category}</h2>
+        <p id="text">${toggledResource.text}</p>
+        <ul>
+            ${toggledResource.sources.map(source => `<li><a href="${source.url}">${source.title}</a></li>`
+            ).join("")}
+        </ul>
+    `
+// Lager en tom string som vi skal fylle med HTML for innholdet.
 
+document.getElementById("content").innerHTML = contentHTML // Oppdaterer innholdet i contentHTML.
+}
+content("HTML") // Kjører funksjonen content med kategorien "HTML" som argument.
